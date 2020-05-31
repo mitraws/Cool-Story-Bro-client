5facb65629c58a56267acb06d7b0de23add4104e
Mitraws <m.ws@outlook.com>
2020-05-31 15:45:16 +0200

    Add reducers from pages to rootReducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import appState from "./appState/reducer";
 import user from "./user/reducer";
-import database from "./database/reducer"
+import homepage from "./homepage/reducer"
+import detail from "./detail/reducer"
 
 export default combineReducers({
   appState,
   user,
-  database
+  homepage,
+  detail
 });
